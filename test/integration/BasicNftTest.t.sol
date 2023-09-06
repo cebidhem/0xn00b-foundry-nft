9ee56d842105f69b7788d7dc1ec0e8e4af257eac
Mehdi Bechiri <cebidhem@pm.me>
2023-09-06 21:30:32 +0200

    Initial Commit
    Signed-off-by: Mehdi Bechiri <cebidhem@pm.me>

---
+++ test/integration/BasicNftTest.t.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {BasicNft} from "../../src/BasicNft.sol";
+import {DeployBasicNft} from "../../script/DeployBasicNft.s.sol";
+
+contract BasicNftTest is Test {
+    DeployBasicNft public deployer;
+    BasicNft public basicNft;
+
+    address public USER = makeAddr("user");
+    string public constant PUG =
+        "ipfs://bafybeicjndupkyakmfbpcgkecykegmjph4nyvu5is5r4fvigkse57u2hna/?filename=0-PUG.json";
+
+    function setUp() public {
+        deployer = new DeployBasicNft();
+        basicNft = deployer.run();
+    }
+
+    function testNameIsCorrect() public view {
+        string memory actualName = basicNft.name();
+        string memory expectedName = "Dogie";
+        // assert(expectedName == actualName); can't compare strings
+        // hash string to compare
+        assert(
+            keccak256(abi.encodePacked(expectedName)) ==
+                keccak256(abi.encodePacked(actualName))
+        );
+    }
+
+    function testCanMintAndHaveABalance() public {
+        vm.prank(USER);
+        basicNft.mintNft(PUG);
+
+        assert(basicNft.balanceOf(USER) == 1);
+        assert(
+            keccak256(abi.encodePacked(basicNft.tokenURI(0))) ==
+                keccak256(abi.encodePacked(PUG))
+        );
+    }
+}
